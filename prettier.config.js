fa3e6c87740f0ffdb03fda39a6e07e154ed73d94
Guney Unay <guneymarketing17@hotmail.com>
2025-02-17 00:01:14 +0300

    chore: add husky and other devolopment stuff

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,5 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
-export default {
+module.exports = {
   $schema: 'https://json.schemastore.org/prettierrc',
   arrowParens: 'always',
   bracketSpacing: true,
