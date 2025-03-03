752023af45ae9d2687709ee63d77130b956f2e64
Guney Unay <guneymarketing17@hotmail.com>
2025-03-03 22:54:46 +0300

    fix: update baseURL to use NEXT_PUBLIC_BETTER_AUTH_URL for production

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from 'better-auth/react'
 
 export const authClient = createAuthClient({
-  baseURL: `${process.env.NODE_ENV === 'production' ? `${process.env.BETTER_AUTH_URL}` : 'http://localhost:3000'}`,
+  baseURL: `${process.env.NODE_ENV === 'production' ? `${process.env.NEXT_PUBLIC_BETTER_AUTH_URL}` : 'http://localhost:3000'}`,
 })
