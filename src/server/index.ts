4e70519da8011918ec84c0d37828be867e44199c
Guney Unay <guneymarketing17@hotmail.com>
2025-03-08 22:37:01 +0300

    fix: simplify CORS configuration by using default settings

--- src/server/index.ts
+++ src/server/index.ts
@@ -13,22 +13,8 @@ import { postRouter } from './routers/post-router'
  */
 const api = j
   .router()
-  .use(
-    cors({
-      origin: [
-        'http://localhost:3000',
-        'http://localhost:8080',
-        'https://product-feedback-app-pied.vercel.app',
-        'https://97aa9034-product-feedback-app.bery.workers.dev',
-      ],
-      allowHeaders: ['x-is-superjson', 'Content-Type', 'Authorization'],
-      exposeHeaders: ['x-is-superjson', 'Content-Length'],
-      allowMethods: ['GET', 'POST', 'OPTIONS' /* , "DELETE", "PUT" */],
-      credentials: true,
-      maxAge: 600,
-    })
-  )
   .basePath('/api')
+  .use(j.defaults.cors)
   .on(['POST', 'GET'], '/auth/**', (c) => auth.handler(c.req.raw))
   .onError(j.defaults.errorHandler)
 /**
