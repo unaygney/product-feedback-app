7ba7a88fdf04306956302bb71930dad7985ca14b
Guney Unay <guneymarketing17@hotmail.com>
2025-03-13 00:56:48 +0300

    feat: add secret page with user data fetching and loading state

--- src/app/secret/page.tsx
+++ src/app/secret/page.tsx
@@ -1,5 +1,31 @@
+'use client'
+
+import { useQuery } from '@tanstack/react-query'
+import { Loader2 } from 'lucide-react'
 import React from 'react'
 
+import { client } from '@/lib/client'
+
 export default function SecretPage() {
-  return <div>This Page Secret</div>
+  const { data: userData, isPending: loading } = useQuery({
+    queryKey: ['get-recent-user'],
+    queryFn: async () => {
+      const res = await client.user.get.$get()
+      return await res.json()
+    },
+  })
+
+  if (loading)
+    return (
+      <div className="flex min-h-screen w-full items-center justify-center">
+        <Loader2 className="h-12 w-12 animate-spin text-blue-500" />
+      </div>
+    )
+
+  return (
+    <div className="flex flex-col gap-4">
+      <h3 className="text-2xl">This page secret</h3>
+      <pre>{JSON.stringify(userData, null, 2)}</pre>
+    </div>
+  )
 }
