77272e33dba87414191992a3b0c3c1449dc66b0d
Guney Unay <guneymarketing17@hotmail.com>
2025-04-01 00:59:21 +0300

    feat: implement product creation and retrieval in product-router

--- src/server/routers/user-router.ts
+++ src/server/routers/user-router.ts
@@ -1,4 +1,7 @@
+import { user as User } from '../db/schema'
 import { j, privateProcedure } from '../jstack'
+import { eq } from 'drizzle-orm'
+import { z } from 'zod'
 
 export const userRouter = j.router({
   get: privateProcedure.query(async ({ c, ctx }) => {
@@ -6,4 +9,35 @@ export const userRouter = j.router({
 
     return c.superjson(user ?? null)
   }),
+  update: privateProcedure
+    .input(
+      z.object({
+        name: z
+          .string()
+          .min(2, { message: 'Name must be at least 2 characters' })
+          .max(100),
+        image: z
+          .string()
+          .url({ message: 'Please enter a valid URL' })
+          .optional()
+          .or(z.literal('')),
+      })
+    )
+    .mutation(async ({ c, ctx, input }) => {
+      const { db, user } = ctx
+      const { name, image } = input
+
+      if (!user) {
+        return c.json({ message: 'User not found' }, 404)
+      }
+
+      try {
+        await db.update(User).set({ name, image }).where(eq(User.id, user.id))
+      } catch (e: unknown) {
+        console.error(e)
+        return c.json({ message: 'Error updating user' }, 500)
+      }
+
+      return c.superjson('User updated!', 200)
+    }),
 })
