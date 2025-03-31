93d9062ada8387a0c0e1ebde33c208aacff6d8e9
Guney Unay <guneymarketing17@hotmail.com>
2025-03-31 20:07:49 +0300

    feat: implement forgot password email functionality with error handling

--- src/server/routers/send-email.ts
+++ src/server/routers/send-email.ts
@@ -1,42 +1,40 @@
-// import { j, publicProcedure } from '../jstack'
-// import { HTTPException } from 'hono/http-exception'
-// import React from 'react'
-// import { z } from 'zod'
+import ForgotPassword from '../../components/emails/forgot-password'
+import { j, publicProcedure } from '../jstack'
+import { HTTPException } from 'hono/http-exception'
+import { z } from 'zod'
 
-// import { resend } from '@/lib/resend'
+export const sendEmailRouter = j.router({
+  forgetPassword: publicProcedure
+    .input(
+      z.object({
+        email: z.string().email('Please enter a valid email address'),
+        subject: z.string().optional(),
+        refreshLink: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, c, input }) => {
+      const { email, refreshLink } = input
+      const { resend } = ctx
 
-// export const sendEmailRouter = j.router({
-//   send: publicProcedure
-//     .input(
-//       z.object({
-//         email: z.string().email('Please enter a valid email address'),
-//         subject: z.string().optional(),
-//         template: z.any(),
-//       })
-//     )
-//     .mutation(async ({ ctx, c, input }) => {
-//       const { email, template } = input
+      try {
+        const { error } = await resend.emails.send({
+          from: 'Product Feedback App <products@guneyunay.com>',
+          to: email,
+          subject: input.subject || 'Product Feedback App',
+          react: ForgotPassword({ refreshLink }),
+        })
+        if (error) {
+          throw new HTTPException(500, {
+            message: 'Error sending email',
+          })
+        }
+      } catch (error) {
+        console.error(error)
+        throw new HTTPException(500, {
+          message: 'Error sending email',
+        })
+      }
 
-//       try {
-//         const { error } = await resend.emails.send({
-//           from: 'Product Feedback App <products@guneyunay.com>',
-//           to: email,
-//           subject: input.subject || 'Product Feedback App',
-//           react: template,
-//         })
-
-//         if (error) {
-//           throw new HTTPException(500, {
-//             message: 'Error sending email',
-//           })
-//         }
-//       } catch (error) {
-//         console.error(error)
-//         throw new HTTPException(500, {
-//           message: 'Error sending email',
-//         })
-//       }
-
-//       return c.json({ message: 'Email sent!' }, 200)
-//     }),
-// })
+      return c.json({ message: 'Email sent!' }, 200)
+    }),
+})
