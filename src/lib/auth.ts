ef683e0899ac8ff2220549617ea660eecc02e652
Guney Unay <guneymarketing17@hotmail.com>
2025-03-31 20:12:22 +0300

    feat: add support for forgot and reset password pages in authentication flow

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 import { captcha } from 'better-auth/plugins'
 
+import { client } from './client'
 import { db } from '@/server/db'
 
 export const auth = betterAuth({
@@ -11,6 +12,13 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
     autoSignIn: true,
+    sendResetPassword: async ({ user, url, token }, request) => {
+      await client.send.forgetPassword.$post({
+        email: user.email,
+        refreshLink: url,
+        subject: 'Product Feedback App | Reset Password',
+      })
+    },
   },
   plugins: [
     captcha({
@@ -25,48 +33,3 @@ export const auth = betterAuth({
     useSecureCookies: process.env.NODE_ENV === 'production',
   },
 })
-
-// import { render } from '@react-email/render'
-// import { betterAuth } from 'better-auth'
-// import { drizzleAdapter } from 'better-auth/adapters/drizzle'
-// import { captcha } from 'better-auth/plugins'
-// import React from 'react'
-
-// import ForgotPasswordEmail from '@/components/emails/forgot-password-email'
-
-// import { client } from './client'
-// import { db } from '@/server/db'
-
-// export const auth = betterAuth({
-//   database: drizzleAdapter(db, {
-//     provider: 'pg',
-//   }),
-//   emailAndPassword: {
-//     enabled: true,
-//     autoSignIn: true,
-//     sendResetPassword: async ({ user, url, token }, request) => {
-//       // Render bileşeni HTML'e çeviriyoruz
-//       const html = render(
-//         React.createElement(ForgotPasswordEmail, { refreshLink: url })
-//       )
-
-//       await client.send.send.$post({
-//         email: user.email,
-//         subject: 'Reset your password',
-//         template: html,
-//       })
-//     },
-//   },
-//   plugins: [
-//     captcha({
-//       provider: 'cloudflare-turnstile',
-//       secretKey:
-//         process.env.NODE_ENV === 'production'
-//           ? process.env.TURNSTILE_SECRET_KEY
-//           : '1x0000000000000000000000000000000AA',
-//     }),
-//   ],
-//   advanced: {
-//     useSecureCookies: process.env.NODE_ENV === 'production',
-//   },
-// })
